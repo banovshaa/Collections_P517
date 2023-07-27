caba5ae94a2e9c1ea2b92fcb87bafac750a34f70
banovshaabdiyeva <abdiyeva.banovsha@gmail.com>
2023-07-27 21:31:25 +0400

    added

---
+++ Collections/Library.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Library
+{
+    public List<Book> books = new List<Book>();
+    public void AddBook(string title, string author, int year)
+    {
+        Book book = new Book(title, author, year);
+        books.Add(book);
+        Console.WriteLine("Book added successfully");
+        //Console.WriteLine("Kitabin adini daxil edin:");
+        //string title = Console.ReadLine();
+        //Console.WriteLine("Kitabin authorunun daxil edin:");
+        //string author = Console.ReadLine();
+        //Console.WriteLine("Kitabin ilini daxil edin:");
+        //int author = Console.ReadLine();
+    }
+    public void RemoveBook(int index)
+    {
+        if (index<books.Count && index>=0)
+        {
+            books.RemoveAt(index);
+            Console.WriteLine("Book is removed");
+        }
+        else
+        {
+            Console.WriteLine("Incorrect index");
+        }
+    }
+    public void DisplayBook()
+    {
+        Console.WriteLine("All books:");
+       for (int i = 0; i < books.Count; i++)
+        {
+            Console.WriteLine(books[i]);
+        }
+    }
+}
+
---
+++ Collections/Program.cs
@@ -0,0 +1,76 @@
+﻿bool isFinished  = false;
+Library library = new Library();
+
+while (!isFinished)
+{
+    Console.WriteLine("\nMenu");
+    Console.WriteLine("1.Add Book");
+    Console.WriteLine("2.Remove Book");
+    Console.WriteLine("3.Display Books");
+    Console.WriteLine("4.Exit");
+
+    Console.Write("Enter your choice:");
+    int choice;
+    int.TryParse(Console.ReadLine(), out choice);
+
+    switch (choice)
+    {
+        case 1:
+            newBookTitle:
+            Console.WriteLine("Type book name:");
+            string title = Console.ReadLine();   
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                Console.WriteLine("Enter title correctly");
+                goto newBookTitle;
+            }
+            newBookAuthor:
+            Console.WriteLine("Type book author:");
+            string author = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(author))
+            {
+                Console.WriteLine("Enter author correctly");
+                goto newBookAuthor;
+            }
+             newBookYear:
+            Console.WriteLine("Type book year:");
+            int year;
+            if (int.TryParse(Console.ReadLine(), out year))
+            {
+                
+                    library.AddBook(title, author, year);
+                
+            }
+            else
+            {
+                    Console.WriteLine("Enter correctly");
+                    goto newBookYear;
+               
+            }
+            
+          
+            break;
+            case 2:
+            Console.WriteLine("Enter book index you want to remove:");
+            int index;
+            if (int.TryParse(Console.ReadLine(), out index))
+            {
+                library.RemoveBook(index+1);
+            }
+            else
+            {
+                Console.WriteLine("Enter correctly");
+            }
+           
+            break;
+            case 3:
+            library.DisplayBook();
+            break;
+            case 4:
+            isFinished = true;
+            break;
+        default:
+            Console.WriteLine("Enter correct choice");
+            break;
+    }
+}
